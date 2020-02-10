3b28c7bf399b1e5248f014f2ff9bf6a539848e5a
erpflueger <erpflueger29@gmail.com>
2020-02-10 17:53:20 -0600

    homeworkfor2 10

---
+++ index.js
@@ -0,0 +1,16 @@
+const whatever = {
+  firstName: "Erika",
+  lastName: "Beaman",
+  favmovie: "sandlot",
+  favsong: "ring of fire",
+  key5: "something",
+  key6: "somethingelse",
+  key7: "thing1",
+  key8: "thing2",
+  key9: "thing3",
+  key10: "thing4",
+}
+
+whatever.newkey = "taco"
+whatever.key10 = "newvalue"
+whatever["key5"] = "newvalue"
